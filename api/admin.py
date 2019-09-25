68266ccd872c396023fc8bcf1424a5fabfd37281
Sebastian Bayerl <me@sebastianbayerl.de>
2019-09-25 10:06:48 +0200

    add models to admin backend

--- api/admin.py
+++ api/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import SpeechEx, Metadata, Medication
+from .models import SpeechEx, Metadata, Medication, TappingEx, MovementEx
 
 admin.site.register(SpeechEx)
 admin.site.register(Metadata)
 admin.site.register(Medication)
+admin.site.register(TappingEx)
+admin.site.register(MovementEx)
