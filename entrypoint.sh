c340a95932e8721e4c91f9c3c45500d82e7ff529
Sebastian Bayerl <me@sebastianbayerl.de>
2019-09-26 16:30:59 +0200

    add working settings and compose + dovkerisation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 
 # stuff that should run before Django starts goes here
-python ./manage.py makemigrations --settings=sma_rest.settings.$1
-python ./manage.py migrate --settings=sma_rest.settings.$1
+python ./manage.py makemigrations --settings=sma_rest.settings
+python ./manage.py migrate --settings=sma_rest.settings
 # load local admin with credentials: base830 and password base830
-python ./manage.py loaddata sma_rest/settings/admin/local_admin.json --settings=sma_rest.settings.$1
+DB_FILE=/srv/appdata/sqlite/db.sqlite
+if [ -f "$DB_FILE" ]; then
+  echo "$DB_FILE exists, doing nothing"
+else
+  echo "$DB_FILE does not exist, creating first user"
+  python ./manage.py loaddata sma_rest/admin/local_admin.json
+fi
+
 
 exec $@
